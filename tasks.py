b46c0c76c406efb949f85cde56e3259996521f01
Pii Sainio <pii.sainio@helsinki.fi>
2026-09-16 13:32:44 +0300

    update tasks

--- tasks.py
+++ tasks.py
@@ -7,7 +7,7 @@ def start(ctx):
     if platform == "win32":
         run("python3 src/index.py", check=True)
     else:
-        run("python3 src/index.py", check=True)
+        ctx.run("python3 src/index.py")
 
 @task
 def test(ctx):
